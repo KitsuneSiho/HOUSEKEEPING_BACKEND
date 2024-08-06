f8014421851c1a32c6ac71fed0492d0913ecdcd1
harry <hw0187@naver.com>
2024-08-07 00:34:45 +0900

    CustomOAuth2UserService 레벨 시스템 도입

--- src/main/java/com/housekeeping/service/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/housekeeping/service/oauth2/CustomOAuth2UserService.java
@@ -6,9 +6,9 @@ import com.housekeeping.entity.LevelEXPTable;
 import com.housekeeping.entity.User;
 import com.housekeeping.entity.enums.Role;
 import com.housekeeping.entity.enums.UserPlatform;
+import com.housekeeping.repository.LevelEXPTableRepository;
 import com.housekeeping.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -23,9 +23,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
-
-    @Autowired
-    private LevelEXPTableRepository levelEXPTableRepository;
+    private final LevelEXPTableRepository levelEXPTableRepository;
 
     @Transactional
     @Override
@@ -36,19 +34,19 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2Response response = null;
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
-        if (clientName.equals("naver")) {
+        if (clientName.equalsIgnoreCase("naver")) {
             response = new NaverResponse(attributes);
-        } else if (clientName.equals("google")) {
+        } else if (clientName.equalsIgnoreCase("google")) {
             response = new GoogleResponse(attributes);
-        } else if (clientName.equals("kakao")) {
+        } else if (clientName.equalsIgnoreCase("kakao")) {
             response = new KakaoResponse(attributes);
         } else {
-            return null;
+            throw new OAuth2AuthenticationException("Unsupported OAuth2 provider");
         }
 
         String providerId = response.getProviderId();
         String provider = response.getProvider();
-        String nickname = provider + "_" + providerId;  // 예: "google_123456789"
+        String nickname = provider + "_" + providerId;
         User userEntity = saveOrUpdateUser(response, nickname, provider);
 
         UserDTO oAuth2UserDto = UserDTO.builder()
@@ -71,11 +69,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             userEntity.setEmail(response.getEmail());
             userEntity.setPhoneNumber(response.getPhoneNumber());
         } else {
-            // 기본 레벨 가져오기 (예: 레벨 1)
-            LevelEXPTable defaultLevel = levelEXPTableRepository.findByLevelLevel(1);
-            if (defaultLevel == null) {
-                throw new RuntimeException("기본 레벨을 찾을 수 없습니다.");
-            }
+            LevelEXPTable defaultLevel = getOrCreateDefaultLevel();
 
             userEntity = User.builder()
                     .username(nickname)
@@ -86,11 +80,23 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                     .userPlatform(UserPlatform.valueOf(provider.toUpperCase()))
                     .role(Role.USER)
                     .userEnrollment(LocalDateTime.now())
-                    .level(defaultLevel)  // 기본 레벨 설정
-                    .userEXP(0)  // 초기 경험치 설정
+                    .level(defaultLevel)
+                    .userEXP(0)
                     .build();
         }
 
         return userRepository.save(userEntity);
+    }
+
+    private LevelEXPTable getOrCreateDefaultLevel() {
+        LevelEXPTable defaultLevel = levelEXPTableRepository.findByLevelLevel(1);
+        if (defaultLevel == null) {
+            defaultLevel = new LevelEXPTable();
+            defaultLevel.setLevelLevel(1);
+            defaultLevel.setLevelName("Beginner");
+            defaultLevel.setLevelRequireEXP(0);
+            defaultLevel = levelEXPTableRepository.save(defaultLevel);
+        }
+        return defaultLevel;
     }
 }
