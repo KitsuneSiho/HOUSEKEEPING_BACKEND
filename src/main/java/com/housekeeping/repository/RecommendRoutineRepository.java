606a145c38345cc000e0690b493ebaefa1ec2686
answodud <answodud0112@gmail.com>
2024-08-09 17:22:08 +0900

    루틴 기능 수정

---
+++ src/main/java/com/housekeeping/repository/RecommendRoutineRepository.java
@@ -0,0 +1,7 @@
+package com.housekeeping.repository;
+
+import com.housekeeping.entity.RecommendRoutine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendRoutineRepository extends JpaRepository<RecommendRoutine, Long> {
+}
