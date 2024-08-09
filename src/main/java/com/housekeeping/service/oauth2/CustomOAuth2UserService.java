219a01d08b8a7a2c6cb08429a6d7b347695b3788
harry <hw0187@naver.com>
2024-08-09 14:15:05 +0900

    토큰값 수정

--- src/main/java/com/housekeeping/service/oauth2/CustomOAuth2UserService.java
+++ src/main/java/com/housekeeping/service/oauth2/CustomOAuth2UserService.java
@@ -2,6 +2,7 @@ package com.housekeeping.service.oauth2;
 
 import com.housekeeping.DTO.UserDTO;
 import com.housekeeping.DTO.oauth2.*;
+import com.housekeeping.entity.User;
 import com.housekeeping.entity.enums.UserPlatform;
 import com.housekeeping.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -40,27 +42,44 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                 throw new OAuth2AuthenticationException("Unsupported provider: " + clientName);
         }
 
-        String providerId = oAuth2Response.getProviderId();
         String email = oAuth2Response.getEmail();
         String name = oAuth2Response.getName();
-        String phoneNumber = oAuth2Response.getPhoneNumber();
+        final String nickname = oAuth2Response.getNickname() != null && !oAuth2Response.getNickname().trim().isEmpty()
+                ? oAuth2Response.getNickname()
+                : clientName + "_" + oAuth2Response.getProviderId();
 
-        // Generate a nickname if it's not provided
-        String nickname = oAuth2Response.getNickname();
-        if (nickname == null || nickname.trim().isEmpty()) {
-            nickname = clientName + "_" + providerId;
+        UserPlatform userPlatform = UserPlatform.valueOf(clientName.toUpperCase());
+
+        // 사용자 정보를 DB에서 가져오거나 새로 생성
+        Optional<User> userOptional = userRepository.findByEmailAndUserPlatform(email, userPlatform);
+        User user;
+
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+        } else {
+            user = createNewUser(email, name, nickname, userPlatform);
         }
-        String role = "ROLE_USER";
 
         UserDTO userDTO = UserDTO.builder()
+                .userId(user.getUserId())
                 .email(email)
                 .name(name)
-                .nickname(nickname)
-                .phoneNumber(phoneNumber)
-                .role(role)
-                .userPlatform(UserPlatform.valueOf(clientName.toUpperCase()))
+                .nickname(user.getNickname())
+                .role(user.getRole())
+                .userPlatform(user.getUserPlatform())
                 .build();
 
         return new CustomOAuth2User(userDTO);
+    }
+
+    private User createNewUser(String email, String name, String nickname, UserPlatform userPlatform) {
+        User newUser = User.builder()
+                .email(email)
+                .name(name)
+                .nickname(nickname)
+                .userPlatform(userPlatform)
+                .role("ROLE_USER")
+                .build();
+        return userRepository.save(newUser);
     }
 }
