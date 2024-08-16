7c0d8d84e51609c80ec47a83c039a65349a5c184
jihun <djawlgns7@gmail.com>
2024-08-16 15:39:44 +0900

    호스팅

--- src/main/java/com/housekeeping/config/SecurityConfig.java
+++ src/main/java/com/housekeeping/config/SecurityConfig.java
@@ -130,7 +130,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","http://192.168.0.42:5000", "http://223.130.129.176:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","http://192.168.0.42:5000", "http://223.130.129.176:3000",
+                "http://223.130.151.138/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true); // 자격 증명을 허용하도록 설정
