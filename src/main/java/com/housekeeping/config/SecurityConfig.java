377c881bad1b01b3f880077d5790b13a5eadee8d
harry <hw0187@naver.com>
2024-08-20 11:43:30 +0900

    시큐리티 권한 설정 추가

--- src/main/java/com/housekeeping/config/SecurityConfig.java
+++ src/main/java/com/housekeeping/config/SecurityConfig.java
@@ -8,8 +8,6 @@ import com.housekeeping.repository.UserRepository;
 import com.housekeeping.service.RefreshTokenService;
 import com.housekeeping.service.UserService;
 import com.housekeeping.service.oauth2.CustomOAuth2UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -28,7 +25,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -36,120 +32,116 @@ import java.util.Collections;
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final JWTUtil jwtUtil;
     private final CustomOAuth2UserService customOAuth2UserService;
     private final RefreshTokenService refreshTokenService;
     private final RefreshRepository refreshRepository;
     private final UserService userService;
 
+    // 비밀번호 암호화를 위한 BCryptPasswordEncoder 빈 등록
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // 인증 실패 시 처리 핸들러
     @Bean
     public AuthenticationFailureHandler authenticationFailureHandler() {
-        return new AuthenticationFailureHandler() {
-            @Override
-            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                System.out.println("exception = " + exception);
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            }
+        return (request, response, exception) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
         };
     }
 
+    // Spring Security 필터 체인 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, UserRepository userRepository) throws Exception {
-        // disable
         http
-                .httpBasic((basic) -> basic.disable())
-                .csrf((csrf) -> csrf.disable());
+                // HTTP Basic 인증 및 CSRF 보호 비활성화
+                .httpBasic(basic -> basic.disable())
+                .csrf(csrf -> csrf.disable())
 
-        // form
-        http
-                .formLogin((form) -> form.disable());
+                // 폼 로그인 비활성화
+                .formLogin(form -> form.disable())
 
-        // oauth2
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .loginPage("/login")
-                        .userInfoEndpoint((userinfo) -> userinfo
-                                .userService(customOAuth2UserService))
-                        .successHandler(new CustomOAuth2SuccessHandler(jwtUtil, refreshTokenService, userService))
-                        .failureHandler(authenticationFailureHandler())
-                        .permitAll());
+                // OAuth2 로그인 설정
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login") // 로그인 페이지 설정
+                        .userInfoEndpoint(userinfo -> userinfo.userService(customOAuth2UserService)) // 사용자 정보 처리 서비스 설정
+                        .successHandler(new CustomOAuth2SuccessHandler(jwtUtil, refreshTokenService, userService)) // 로그인 성공 시 처리 핸들러
+                        .failureHandler(authenticationFailureHandler()) // 로그인 실패 시 처리 핸들러
+                        .permitAll())
 
-        // logout
-        http
+                // 로그아웃 설정
                 .logout(logout -> logout
                         .logoutSuccessHandler((request, response, authentication) -> {
                             response.setStatus(HttpServletResponse.SC_OK);
                         })
-                        .logoutUrl("/api/auth/logout")
-                        .deleteCookies("JSESSIONID", "refresh")
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                );
+                        .logoutUrl("/api/auth/logout") // 로그아웃 URL 설정
+                        .deleteCookies("JSESSIONID", "refresh") // 로그아웃 시 쿠키 삭제
+                        .invalidateHttpSession(true) // 세션 무효화
+                        .clearAuthentication(true)) // 인증 정보 제거
 
-        // cors
-        http
-                .cors((cors) -> cors.configurationSource(corsConfigurationSource()));
+                // CORS 설정
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
-        // authorization
-        http.authorizeHttpRequests((auth) -> auth
-                .requestMatchers("/", "/login", "/logout", "/oauth2-jwt-header", "/api/auth/complete-registration").permitAll()
-                .requestMatchers("/firstlogin", "/firstmain", "/firstlivingroom", "/firstroomdesign", "firsttoiletroom").permitAll()
-                .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/reissue", "/reissue/socket").permitAll() // /reissue 엔드포인트를 인증 없이 접근 허용
-                .requestMatchers("/api/user/**").authenticated()
-                .requestMatchers("/mainpage").hasRole("USER")
+                // 엔드포인트별 권한 설정
+                .authorizeHttpRequests(auth -> auth
+                        // 공용 엔드포인트 허용
+                        .requestMatchers("/", "/login", "/logout", "/oauth2-jwt-header", "/api/auth/complete-registration").permitAll()
+                        .requestMatchers("/firstlogin", "/firstmain", "/firstlivingroom", "/firstroomdesign", "firsttoiletroom").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/reissue", "/reissue/socket").permitAll()
 
-
-                .requestMatchers(HttpMethod.POST, "/api/posts/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("ADMIN")
-                .requestMatchers(HttpMethod.GET, "/api/posts/**").permitAll()
+                        // ROLE_ADMIN 권한이 필요한 엔드포인트
+                        .requestMatchers(HttpMethod.POST, "/api/tips/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/api/tips/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/tips/**").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/api/tips/**").hasAnyRole("USER")
 
 
-                .requestMatchers("/admin").hasRole("ADMIN")
+                        // ROLE_ADMIN 권한이 필요한 엔드포인트
+                        .requestMatchers(HttpMethod.POST, "/api/posts/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/posts/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/posts/**").hasRole("ADMIN")
 
-                .anyRequest().authenticated());
+                        // ROLE_USER 이상 권한이 필요한 엔드포인트
+                        .requestMatchers(HttpMethod.GET, "/api/posts/**").hasAnyRole("USER", "ADMIN")
 
-        // 인가되지 않은 사용자에 대한 exception -> 프론트엔드로 코드 응답
-        http.exceptionHandling((exception) ->
-                exception
-                        .authenticationEntryPoint((request, response, authException) -> {
+                        // 관리자 페이지 접근 권한 설정
+                        .requestMatchers("/admin").hasRole("ADMIN")
+
+                        // 기타 모든 요청은 인증된 사용자만 허용
+                        .anyRequest().authenticated())
+
+                // 인증되지 않은 요청에 대한 예외 처리
+                .exceptionHandling(exception ->
+                        exception.authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                        }));
+                        }))
 
-        // jwt filter
-        http
-                .addFilterAfter(new JWTFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
+                // JWT 필터 추가
+                .addFilterAfter(new JWTFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class)
 
-        // custom logout filter 등록
-        http
-                .addFilterBefore(new JWTFilter(jwtUtil, userRepository), UsernamePasswordAuthenticationFilter.class);
-
-        // session stateless
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                // 세션 상태 관리: STATELESS로 설정 (세션을 사용하지 않음)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
 
+    // CORS 설정 빈 등록
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000","http://192.168.0.42:5000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://192.168.0.42:5000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
-        configuration.setAllowCredentials(true); // 자격 증명을 허용하도록 설정
+        configuration.setAllowCredentials(true); // 자격 증명을 허용
         configuration.setExposedHeaders(Arrays.asList("access", "Authorization"));
-        configuration.setMaxAge(3600L);
+        configuration.setMaxAge(3600L); // CORS 응답 캐싱 시간 설정
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
