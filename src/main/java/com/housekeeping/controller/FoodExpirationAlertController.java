e55b2056b2cc37e95d52a91810a28a0156405965
sangwoo <whiz10@naver.com>
2024-08-23 21:56:00 +0900

    유통기한 알림 시연중(알림시간 22시)

--- src/main/java/com/housekeeping/controller/FoodExpirationAlertController.java
+++ src/main/java/com/housekeeping/controller/FoodExpirationAlertController.java
@@ -44,7 +44,7 @@ public class FoodExpirationAlertController {
         this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
     }
 
-    @Scheduled(cron = "0 00 20 * * ?")
+    @Scheduled(cron = "0 00 22 * * ?")
     public void checkFoodExpirationAndSendAlerts() {
         logger.info("checkFoodExpirationAndSendAlerts method started.");
         LocalDate today = LocalDate.now();
